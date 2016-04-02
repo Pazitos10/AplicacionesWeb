9c47489ade9c217a277d4ab36f35f51075c1483a
Pazitos10 <pazosbruno@hotmail.com>
2016-04-02 02:32:55 -0300

    avances en busqueda por titulo

--- static/js/buscar_peliculas.js
+++ static/js/buscar_peliculas.js
@@ -70,54 +70,100 @@ $(document).ready(function () {
             });
     }
 
+    /**
+    * Devuelve los generos de una pelicula con un formato legible
+    * @param Array genres
+    */
+    function get_genres_name(genres_array){
+        var genres = '';
+        for (var i in genres_array) {
+            if (genres !== ''){
+                genres += " | " +genres_array[i].name;
+            }else {
+                genres += genres_array[i].name;
+            }
+        }
+        return genres;
+    }
+
+
+    function autocomplete_succeed(data){
+        limpiar_form();
+        if (data.hasOwnProperty('total_results')){
+            if (data.total_results > 0){
+                data = data.results[0];
+            }else{
+                autocomplete_failed();
+                return false;
+            }
+        }
+        var form_inputs = $('#form-inputs');
+        var titulo = data.original_title;
+        //TODO: Verificar cuando viene por titulo, genres no existe -> genres_ids existe y hay que buscar los nombres a mano
+        var generos = get_genres_name(data.genres);
+        var anio = new Date(String(data.release_date)).getFullYear();
+        var poster_url = "http://placehold.it/150x200?text=Sin+Imagen";
+        if (data.poster_path !== null ){
+            poster_url = "http://image.tmdb.org/t/p/w150" + data.poster_path;
+        }
+        var rating = data.vote_average;
+        if(rating > 5.0){
+            rating = 5.0;
+        }
+        var info_pelicula = "<div class='form-group info-pelicula'>"+
+            "<div class='portada-container'>"+
+                "<img class='portada' id='portada-id' src='"+poster_url+"' alt=''>"+
+            "</div>"+
+            "<div class='puntaje puntaje-form'>"+
+                "<input id='ratings-hidden' name='rating' type='hidden'>"+
+                "<input id='rating' type='hidden' class='rating' data-filled='glyphicon glyphicon-star' data-empty='glyphicon glyphicon-star-empty' data-fractions='2' />"+
+            "</div>"+
+            "<p id='title-display'>"+titulo+"</p>"+
+            "<p id='genre-display'>"+generos+"</p>"+
+            "<p id='year-display'>"+anio+"</p>"+
+        "</div>";
+        form_inputs.after(info_pelicula);
+
+        // Datos para enviar con el form
+        $("#title").attr("value", titulo);
+        $("#genre").attr('value', generos);
+        $("#year").attr("value", anio);
+        $('#rating').rating('rate', rating);
+    }
+
+
+    function autocomplete_failed(){
+        limpiar_form();
+        var form_inputs = $('#form-inputs');
+        var poster_url = "http://i.imgur.com/WCeJfRi.webm";
+        var info_pelicula = "<div class='form-group info-pelicula'>"+
+            "<div class='portada-container'>"+
+                "<video class='portada portada-404' id='portada-id' src='"+poster_url+"' alt='' autoplay loop type='video/webm'>"+
+            "</div>"+"</div>";
+        form_inputs.after(info_pelicula);
+    }
 
     /**
-    * Devuelve la URL con la imagen de poster de la pelicula
+    * Busca informacion de la pelicula en IMDB y la muestra en pantalla
     * @param String imdb_id Id de IMDB
-    * @param String img_id Id del objeto IMG
     */
-    function buscar_info_pelicula(imdb_id){
+    function autocompletar_form(imdb_id){
         var API_KEY = "53eb1914f7a9090c92553339f74280ce";
+        if(imdb_id.startsWith('tt')){
+            //buscamos por id
+            var url = "https://api.themoviedb.org/3/movie/" + imdb_id + "?api_key=" + API_KEY;
+        }else{
+            var titulo = encodeURI(imdb_id);
+            var url  = "http://api.themoviedb.org/3/search/movie?api_key="+ API_KEY+ "&query="+ titulo;
+        }
         $.ajax({
-            url: "https://api.themoviedb.org/3/movie/" + imdb_id + "?api_key=" + API_KEY,
+            url: url,
             method: "GET"
             })
-            .done(function( data ) {
-                console.log(data);
-                //poster
-                $("#portada-id").attr("src", "http://image.tmdb.org/t/p/w150" + data.poster_path);
-
-                //titulo
-                $("#title-display").text(data.original_title);
-                $("#title").attr("value",data.original_title);
-
-                //Genero
-                var genres = '';
-                for (var i in data.genres) {
-                    if (genres !== ''){
-                        genres += $("#genre-display").val() + " | " +data.genres[i].name;
-                    }else {
-                        genres += data.genres[i].name;
-                    }
-                }
-                console.log(genres);
-                $("#genre-display").text(genres);
-                $("#genre").attr('value', genres);
-
-                //Año
-                var anio = new Date(String(data.release_date)).getFullYear();
-                $("#year-display").text(anio);
-                $("#year").attr("value", anio);
-
-                //Rating
-                var rating = data.vote_average;
-                if(rating > 5.0){
-                    rating = 5.0;
-                }
-                $('#rating').rating('rate', rating);
-
-            });
+            .done(function (data) { autocomplete_succeed(data) })
+            .fail(autocomplete_failed());
     }
+
 
     $("#search").click(function () {
         var search_term = $("#search_term").val();
@@ -144,13 +190,16 @@ $(document).ready(function () {
         }
     });
 
-    function autocompletar_form(){
-        buscar_info_pelicula($('#id').val(), 'portada-id');
+
+    function limpiar_form(){
+        $('.form-alta #id').value = '';
+        $('.form-alta .info-pelicula').remove();
     }
 
-
     $('.form-alta #id').change(function () {
+        limpiar_form();
         if ($(this).val() !== '') {
-            autocompletar_form()}
+            autocompletar_form($(this).val())
+        }
     });
 });
