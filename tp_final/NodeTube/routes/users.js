6c486ee15d5e7e96226cad55d0f98e38967b532f
Matias Iglesias <matiasiglesias@meridiem.com.ar>
2016-09-26 10:41:38 -0300

    Instalación y agregado de ruta

--- tp_final/NodeTube/routes/users.js
+++ tp_final/NodeTube/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/profile', function(req, res, next) {
+  res.send('Falta mostrar perfil del usuario');
+});
+
 module.exports = router;
