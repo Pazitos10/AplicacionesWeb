3499f83258d0aa9f3a8fd5793005db1ac18ae4ad
Matias Iglesias <matiasiglesias@meridiem.com.ar>
2017-01-21 11:30:35 -0300

    Primer commit Instalar firebase cli: npm install -g firebase-tools Login: firebase login Deploy (reglas o datos): firebase deploy

---
+++ tp_final/DirectorioEmpresasFireBase/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+
+import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAf_LM_hH28h-FVezIh40XFQtg60lkzcw4",
+  authDomain: "directorioempresas-74f45.firebaseapp.com",
+  databaseURL: "https://directorioempresas-74f45.firebaseio.com",
+  storageBucket: "directorioempresas-74f45.appspot.com",
+  messagingSenderId: "384507057866"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
